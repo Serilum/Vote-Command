936fb7d85629fd15ab24fa43388c925c540f9da0
Rick South <rick@natamus.com>
2023-12-11 23:46:40 +0100

    Vote Command source code for 1.19.2.

---
+++ Common/src/main/java/com/natamus/votecommand/cmds/CommandVote.java
@@ -0,0 +1,24 @@
+package com.natamus.votecommand.cmds;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.natamus.collective.functions.StringFunctions;
+import com.natamus.votecommand.config.ConfigHandler;
+import net.minecraft.ChatFormatting;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.world.entity.player.Player;
+
+public class CommandVote {
+	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+		dispatcher.register(Commands.literal("vote")
+			.requires((iCommandSender) -> iCommandSender.getEntity() instanceof Player)
+			.executes((command) -> {
+				Player player = command.getSource().getPlayer();
+				if (player != null) {
+					StringFunctions.sendMessage(player, ConfigHandler.voteCommandMessage, ChatFormatting.DARK_GREEN);
+				}
+				return 1;
+			})
+		);
+	}
+}
