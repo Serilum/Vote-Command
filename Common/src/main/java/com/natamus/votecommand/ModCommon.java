ebd1322be2c4b21b37d30c305054b62f055b9f6b
Rick South <rick@natamus.com>
2023-12-11 23:46:56 +0100

    Vote Command source code for 1.20.0.

---
+++ Common/src/main/java/com/natamus/votecommand/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.votecommand;
+
+import com.natamus.votecommand.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
