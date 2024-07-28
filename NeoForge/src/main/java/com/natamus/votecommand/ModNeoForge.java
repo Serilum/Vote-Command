30b8448a8e91b98fd536c9dea3d98ab9155ea8ae
Rick South <rick@natamus.com>
2024-07-28 07:09:55 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/votecommand/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/votecommand/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.votecommand;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.votecommand.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.votecommand.neoforge.events.NeoForgeCommandRegisterEvent;
 import com.natamus.votecommand.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
