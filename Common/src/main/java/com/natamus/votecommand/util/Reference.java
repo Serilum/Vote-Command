807e1a3e6d8f29d999f46ec5319377020a328fde
Rick South <rick@natamus.com>
2024-08-09 08:00:52 +0200

    Vote Command source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/votecommand/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.votecommand.util;
+
+public class Reference {
+	public static final String MOD_ID = "votecommand";
+	public static final String NAME = "Vote Command";
+	public static final String VERSION = "3.5";
+	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
+}
