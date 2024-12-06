8147913b208d011b116f37864aa73e9cf4a48b18
Rick South <rick@natamus.com>
2024-12-06 13:08:04 +0100

    Vote Command source code for 1.21.4.

---
+++ Fabric/src/main/java/com/natamus/votecommand/ModFabric.java
@@ -0,0 +1,35 @@
+package com.natamus.votecommand;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.votecommand.cmds.CommandVote;
+import com.natamus.votecommand.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			CommandVote.register(dispatcher);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
