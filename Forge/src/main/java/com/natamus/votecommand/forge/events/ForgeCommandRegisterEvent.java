8e0e6b6e330e6d3f4bf0187fb2d1b606e6d38b5e
Rick South <rick@natamus.com>
2025-02-12 23:08:03 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/votecommand/forge/events/ForgeCommandRegisterEvent.java
+++ Forge/src/main/java/com/natamus/votecommand/forge/events/ForgeCommandRegisterEvent.java
@@ -3,12 +3,10 @@ package com.natamus.votecommand.forge.events;
 import com.natamus.votecommand.cmds.CommandVote;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeCommandRegisterEvent {
     @SubscribeEvent
-    public void registerCommands(RegisterCommandsEvent e) {
+    public static void registerCommands(RegisterCommandsEvent e) {
     	CommandVote.register(e.getDispatcher());
     }
 }
