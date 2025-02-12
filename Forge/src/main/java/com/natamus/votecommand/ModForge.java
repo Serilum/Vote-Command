aa32defb2f25166112ee1687aac89a560f65d5eb
Rick South <rick@natamus.com>
2025-02-12 23:08:08 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/votecommand/ModForge.java
+++ Forge/src/main/java/com/natamus/votecommand/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-		MinecraftForge.EVENT_BUS.register(new ForgeCommandRegisterEvent());
+		MinecraftForge.EVENT_BUS.register(ForgeCommandRegisterEvent.class);
 	}
 
 	private static void setGlobalConstants() {
--- NeoForge/src/main/java/com/natamus/votecommand/neoforge/events/NeoForgeCommandRegisterEvent.java
+++ NeoForge/src/main/java/com/natamus/votecommand/neoforge/events/NeoForgeCommandRegisterEvent.java
@@ -3,9 +3,7 @@ package com.natamus.votecommand.neoforge.events;
 import com.natamus.votecommand.cmds.CommandVote;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeCommandRegisterEvent {
 	@SubscribeEvent
 	public static void registerCommands(RegisterCommandsEvent e) {
